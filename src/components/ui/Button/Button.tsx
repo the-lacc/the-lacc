3d87c5f00181f13e3fa96413f402a6e7931865a9
Mykola Korzhykov <mr.purple2007@gmail.com>
2025-10-30 10:29:50 -0500

    Create First Screen

---
+++ src/components/ui/Button/Button.tsx
@@ -0,0 +1,24 @@
+import { FC, MouseEvent } from 'react';
+
+import anchorClick from '@helpers/anchorClick';
+
+type Props = {
+    type?: 'dark' | 'white'
+    text: string
+    href: string
+    className?: string
+}
+
+const Button: FC<Props> = ({className, text, href, type = 'white'}) => {
+    const anchorHandleClick = (e: MouseEvent<HTMLAnchorElement>) => {
+        anchorClick(e);
+    }
+
+    return (
+        <a href={href} className={`${className} themeButton ${type}`} onClick={anchorHandleClick}>
+            {text}
+        </a>
+    )
+}
+
+export default Button;
